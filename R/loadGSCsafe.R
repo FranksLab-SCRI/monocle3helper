2c0b1eb760ab10cba76b12e7733acb2109e29da0
Jennifer Franks <franksjenniferm@gmail.com>
2026-02-18 13:42:57 -0800

    fix loadGSCsafe

--- R/loadGSCsafe.R
+++ R/loadGSCsafe.R
@@ -16,151 +16,156 @@
 #' }
 #'
 #' @export
-loadGSCSafe <- function(
-    file,
-    type = "auto",
-    addInfo = NULL,
-    sep = "\t",
-    encoding = "latin1"
-) {
-
-  addUserInfo <- if (is.null(addInfo)) "skip" else "yes"
-
-  type <- try(
-    match.arg(type, c("auto", "gmt", "sif", "data.frame")),
-    silent = TRUE
-  )
-  if (inherits(type, "try-error")) {
-    stop("argument 'type' set to unknown value")
+loadGSCSafe <- function (file, type = "auto", addInfo, sep="\t", encoding="latin1")
+{
+  if (missing(addInfo)) {
+    addUserInfo <- "skip"
+    addInfo <- "none"
   }
-
-  ## ----------------------------
-  ## Auto-detect type
-  ## ----------------------------
+  else {
+    addUserInfo <- "yes"
+  }
+  tmp <- try(type <- match.arg(type, c("auto", "gmt", "sbml",
+                                       "sif", "data.frame"), several.ok = FALSE), silent = TRUE)
+  if (class(tmp) == "try-error") {
+    stop("argument type set to unknown value")
+  }
   if (type == "auto") {
-    if (is.character(file)) {
-      ext <- tolower(tail(strsplit(file, "\\.")[[1]], 1))
-      if (!ext %in% c("gmt", "sif")) {
-        stop("Unsupported file extension: ", ext)
-      }
-      type <- ext
-    } else {
+    if (class(file) == "character") {
+      tmp <- unlist(strsplit(file, "\\."))
+      type <- tolower(tmp[length(tmp)])
+      if (!type %in% c("gmt", "sif", "sbml", "xml"))
+        stop(paste("can not handle .", type, " file extension, read manually using e.g. read.delim() and load as data.frame",
+                   sep = ""))
+    }
+    else {
       type <- "data.frame"
     }
   }
-
-  ## ----------------------------
-  ## GMT
-  ## ----------------------------
   if (type == "gmt") {
-
-    # Read all lines at once
-    lines <- readLines(file, encoding = encoding)
-
-    # Split on tab
-    split_lines <- strsplit(lines, sep, fixed = TRUE)
-
-    # Preallocate
-    n <- length(split_lines)
-
-    gsc <- vector("list", n)
-    names(gsc) <- character(n)
-
-    if (addUserInfo == "skip") {
-      addInfo_local <- matrix(NA_character_, n, 2)
+    con <- file(file, encoding=encoding)
+    tmp <- try(suppressWarnings(open(con)), silent = TRUE)
+    if (class(tmp) == "try-error")
+      stop("file could not be read")
+    if (addUserInfo == "skip")
+      addInfo <- vector()
+    gscList <- list()
+    i <- 1
+    tmp <- try(suppressWarnings(while (length(l <- scan(con,
+                                                        nlines = 1, what = "character", quiet = T, sep=sep)) > 0) {
+      if (addUserInfo == "skip")
+        addInfo <- rbind(addInfo, l[1:2])
+      tmp <- l[3:length(l)]
+      gscList[[l[1]]] <- unique(tmp[tmp != "" & tmp !=
+                                      " " & !is.na(tmp)])
+      i <- i + 1
+    }), silent = TRUE)
+    if (class(tmp) == "try-error")
+      stop("file could not be read")
+    close(con)
+    gsc <- gscList[!duplicated(names(gscList))]
+    if (addUserInfo == "skip")
+      addInfo <- unique(addInfo)
+  }
+  else if (type %in% c("sbml", "xml")) {
+    require(rsbml)
+    tmp <- try(sbml <- rsbml_read(file))
+    if (class(tmp) == "try-error") {
+      stop("file could not be read by rsbml_read()")
     }
-
-    for (i in seq_len(n)) {
-
-      line <- split_lines[[i]]
-
-      set_name <- line[1]
-      description <- line[2]
-
-      genes <- line[-c(1, 2)]
-      genes <- genes[genes != "" & !is.na(genes)]
-
-      names(gsc)[i] <- set_name
-      gsc[[i]] <- genes
-
-      if (addUserInfo == "skip") {
-        addInfo_local[i, ] <- c(set_name, description)
+    gsc <- list()
+    for (iReaction in 1:length(reactions(model(sbml)))) {
+      metIDs <- names(c(reactants(reactions(model(sbml))[[iReaction]]),
+                        products(reactions(model(sbml))[[iReaction]])))
+      geneIDs <- names(modifiers(reactions(model(sbml))[[iReaction]]))
+      if (length(geneIDs) > 0) {
+        geneNames <- rep(NA, length(geneIDs))
+        for (iGene in 1:length(geneIDs)) {
+          geneNames[iGene] <- name(species(model(sbml))[[geneIDs[iGene]]])
+        }
+        for (iMet in 1:length(metIDs)) {
+          gsc[[metIDs[iMet]]] <- c(gsc[[metIDs[iMet]]],
+                                   geneNames)
+        }
       }
     }
-
-    # Remove duplicates
-    keep <- !duplicated(names(gsc))
-    gsc <- gsc[keep]
-
-    if (addUserInfo == "skip") {
-      addInfo <- unique(addInfo_local[keep, , drop = FALSE])
-      addInfo <- as.data.frame(addInfo, stringsAsFactors = FALSE)
+    if (length(gsc) == 0) {
+      stop("no gene association found")
+    }
+    else {
+      for (iMet in 1:length(gsc)) {
+        tmp1 <- name(species(model(sbml))[[names(gsc)[iMet]]])
+        tmp2 <- compartment(species(model(sbml))[[names(gsc)[iMet]]])
+        names(gsc)[iMet] <- paste(tmp1, " (", tmp2, ")",
+                                  sep = "")
+      }
     }
   }
-
-  ## ----------------------------
-  ## SIF
-  ## ----------------------------
   else if (type == "sif") {
-
-    gsc_df <- try(
-      utils::read.delim(
-        file,
-        header = FALSE,
-        quote = "",
-        stringsAsFactors = FALSE
-      ),
-      silent = TRUE
-    )
-
-    if (inherits(gsc_df, "try-error") || ncol(gsc_df) != 3) {
-      stop("SIF file must contain exactly three columns")
+    tmp <- try(gsc <- as.data.frame(read.delim(file, header = FALSE,
+                                               quote = "", as.is = TRUE), stringsAsFactors = FALSE),
+               silent = TRUE)
+    if (class(tmp) == "try-error") {
+      stop("argument file could not be read and converted into a data.frame")
     }
-
-    if (addUserInfo == "skip") {
-      addInfo <- gsc_df[, 1:2]
+    if (ncol(gsc) != 3) {
+      stop("sif file should contain three columns")
     }
-
-    gsc_df <- unique(gsc_df[, c(3, 1)])
-    geneSets <- unique(gsc_df[, 2])
-
-    gsc <- lapply(geneSets, function(gs) {
-      gsc_df[gsc_df[, 2] == gs, 1]
-    })
-    names(gsc) <- geneSets
+    if (addUserInfo == "skip")
+      addInfo <- gsc[, c(1, 2)]
+    gsc <- gsc[, c(3, 1)]
+    tmp <- nrow(gsc)
+    gsc <- unique(gsc)
+    geneSets <- unique(gsc[, 2])
+    gscList <- list()
+    for (iGeneSet in 1:length(geneSets)) {
+      gscList[[iGeneSet]] <- gsc[gsc[, 2] == geneSets[iGeneSet],
+                                 1]
+    }
+    names(gscList) <- geneSets
+    gsc <- gscList
   }
-
-  ## ----------------------------
-  ## data.frame
-  ## ----------------------------
   else if (type == "data.frame") {
-
-    gsc_df <- as.data.frame(file, stringsAsFactors = FALSE)
-    if (ncol(gsc_df) != 2) {
-      stop("data.frame input must contain exactly two columns")
+    tmp <- try(gsc <- as.data.frame(file, stringsAsFactors = FALSE),
+               silent = TRUE)
+    if (class(tmp) == "try-error") {
+      stop("argument file could not be converted into a data.frame")
     }
-
-    gsc_df <- unique(gsc_df)
-    geneSets <- unique(gsc_df[, 2])
-
-    gsc <- lapply(geneSets, function(gs) {
-      gsc_df[gsc_df[, 2] == gs, 1]
-    })
-    names(gsc) <- geneSets
-  }
-
-  ## ----------------------------
-  ## Additional info handling
-  ## ----------------------------
-  if (!is.null(addInfo)) {
-    addInfo <- as.data.frame(addInfo, stringsAsFactors = FALSE)
-    if (ncol(addInfo) != 2) {
-      stop("addInfo must contain exactly two columns")
+    for (i in 1:ncol(gsc)) {
+      gsc[, i] <- as.character(gsc[, i])
     }
-    addInfo <- unique(addInfo[addInfo[, 1] %in% names(gsc), ])
+    if (ncol(gsc) != 2) {
+      stop("argument file has to contain exactly two columns")
+    }
+    tmp <- nrow(gsc)
+    gsc <- unique(gsc)
+    geneSets <- unique(gsc[, 2])
+    gscList <- list()
+    for (iGeneSet in 1:length(geneSets)) {
+      gscList[[iGeneSet]] <- gsc[gsc[, 2] == geneSets[iGeneSet],
+                                 1]
+    }
+    names(gscList) <- geneSets
+    gsc <- gscList
   }
-
-  res <- list(gsc = gsc, addInfo = addInfo)
+  if (addUserInfo == "yes") {
+    tmp <- try(addInfo <- as.data.frame(addInfo, stringsAsFactors = FALSE),
+               silent = TRUE)
+    if (class(tmp) == "try-error") {
+      stop("failed to convert additional info in argument 'addInfo' into a data.frame")
+    }
+  }
+  #if (class(addInfo) == "data.frame") {
+  #  if (ncol(addInfo) != 2)
+  #    stop("additional info in argument 'file' or 'addInfo' has to contain 2 columns")
+  #  tmp <- nrow(addInfo)
+  #  addInfo <- unique(addInfo[addInfo[, 1] %in% names(gsc),
+  #                            ])
+  #}
+  else {
+  }
+  res <- list(gsc, addInfo)
+  names(res) <- c("gsc", "addInfo")
   class(res) <- "GSC"
-  res
+  return(res)
 }
